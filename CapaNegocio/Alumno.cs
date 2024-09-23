361efcd5cbca8b5c4d2faff75509bd002393b5b5
EstudianteUC <EstudianteUC@UCUXA60620.continental.edu.pe>
2024-09-23 18:32:17 -0500

    Primer proyecto

---
+++ CapaNegocio/Alumno.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocio
+{
+    public class Alumno
+    {
+        public string CodAlumno { get; set; }
+        public string APaterno { get; set; }
+        public string AMaterno { get; set; }
+        public string Nombres { get; set; }
+        public string Usuario { get; set; }
+        public string CodCarrera { get; set; }
+    }
+}
---
+++ CapaNegocio/Carrera.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocio
+{
+    public class Carrera
+    {
+        //mapear las columnas de la carrera
+        public string CodCarrera { get; set; }
+
+        public string NombreCarrera { get; set; }
+    }
+}
