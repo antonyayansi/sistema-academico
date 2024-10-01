69c365814f2f9707f954ac5364f436e4f6fecb83
EstudianteUC <EstudianteUC@UCUXA60618.continental.edu.pe>
2024-09-30 19:45:11 -0500

    hecho

--- CapaNegocio/Asignatura.cs
+++ CapaNegocio/Asignatura.cs
@@ -4,12 +4,95 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
 namespace CapaNegocio
 {
     public class Asignatura
     {
+        
+
+        public string cadena = ConfigurationManager.ConnectionStrings["Cadena"].ConnectionString;
         public string CodAsignatura { get; set; }
         public string NombreAsignatura { get; set; }
         public string CodRequisito { get; set; }
+
+
+        public DataTable Listar()
+        {
+            using (SqlConnection conexion = new SqlConnection(cadena))
+            {
+                string consulta = "SELECT * FROM TAsignatura";
+                SqlDataAdapter adapter = new SqlDataAdapter(consulta, conexion);
+                DataTable tabla = new DataTable();
+                adapter.Fill(tabla);
+                return tabla;
+            }
+        }
+
+        public bool Agregar()
+        {
+            using (SqlConnection conexion = new SqlConnection(cadena))
+            {
+                string consulta = "INSERT INTO TAsignatura values(@CodCarrera, @NombreCarrera, @CodRequisito)";
+                SqlCommand comando = new SqlCommand(consulta, conexion);
+                comando.Parameters.AddWithValue("@CodAsignatura", CodAsignatura);
+                comando.Parameters.AddWithValue("@NombreAsignatura", NombreAsignatura);
+                conexion.Open();
+                byte i = Convert.ToByte(comando.ExecuteNonQuery());
+                conexion.Close();
+                if (i == 1) return true;
+                else return false;
+            }
+        }
+
+
+        public bool Eliminar()
+        {
+            using (SqlConnection conexion = new SqlConnection(cadena))
+            {
+                string consulta = "DELETE FROM TAsignatura WHERE CodAsignartura= @CodAsignatura";
+                SqlCommand comando = new SqlCommand(consulta, conexion);
+                comando.Parameters.AddWithValue("@CodAsignatura", CodAsignatura);
+                conexion.Open();
+                byte i = Convert.ToByte(comando.ExecuteNonQuery());
+                conexion.Close();
+                if (i == 1) return true;
+                else return false;
+            }
+        }
+
+        public bool Actualizar()
+        {
+            using (SqlConnection conexion = new SqlConnection(cadena))
+            {
+                string consulta = "UPDATE TAsignatura SET Asignatura = @NombreAsignatura WHERE CodAsignatura = @CodAsignatura";
+                SqlCommand comando = new SqlCommand(consulta, conexion);
+                comando.Parameters.AddWithValue("@NombreAsignatura", NombreAsignatura);
+                comando.Parameters.AddWithValue("@CodAsignatura", CodAsignatura);
+                conexion.Open();
+                byte i = Convert.ToByte(comando.ExecuteNonQuery());
+                conexion.Close();
+                if (i == 1) return true;
+                else return false;
+            }
+        }
+
+        public DataTable Buscar()
+        {
+            using (SqlConnection conexion = new SqlConnection(cadena))
+            {
+                string consulta = "SELECT * FROM TAsignatura WHERE CodAsigantura = @CodAsignatura";
+                SqlCommand comando = new SqlCommand(consulta, conexion);
+                comando.Parameters.AddWithValue("@CodAsignatura", CodAsignatura);
+                SqlDataAdapter adapter = new SqlDataAdapter(comando);
+                DataTable tabla = new DataTable();
+                adapter.Fill(tabla);
+                return tabla;
+            }
+
+        }
     }
 }
---
+++ CapaPresentacion/privado/frmAsignatura.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="CapaPresentacion.Privado.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+        <h3>Mantenimiento de la tabla asignatura</h3>
+    <p>
+        CodAsiganatura: <asp:TextBox runat="server" Id="txtCodAsignatura" />
+    </p>
+     <p>
+        Asignatura: <asp:TextBox runat="server" Id="txtAsignatura" />
+    </p>
+    <p>
+        CodRequisito: <asp:TextBox runat="server" Id="TextCodRequisito" />
+    </p>
+    <p>
+        <asp:Button Text="Agregar" runat="server" Id="btnAgregar" OnClick="btnAgregar_Click" />
+        <asp:Button Text="Eliminar" runat="server" Id="btnEliminar" OnClick="btnEliminar_Click"  />
+        <asp:Button Text="Actualizar" runat="server" Id="btnActualizar"  />
+    </p>
+    <p>
+        Buscar: <asp:TextBox runat="server" Id="txtBuscar" />
+        <asp:Button Text="Buscar" runat="server" Id="btnBuscar" OnClick="btnBuscar_Click" />
+    </p>
+    <p>
+        <asp:GridView runat="server" ID="gvCarrera"></asp:GridView>
+    </p>
+</asp:Content>
